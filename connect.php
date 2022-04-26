a7a1ae19b557f1b324938750df144f7a175711b4
JayJajoo <84397446+JayJajoo@users.noreply.github.com>
2022-04-26 22:26:32 +0530

    Added Flask and php scripts

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+    $conn=new mysqli('localhost','root','','SE');
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $message=$_POST['message'];
+    if($conn->connect_error){
+        echo "lodu";
+    }
+    else{
+        $stmt=$conn->prepare("insert into users(name,email,message) values(?,?,?)");
+        $stmt->bind_param("sss",$name,$email,$message);
+        $stmt->execute();
+        echo "submitted successfully...";
+        $stmt->close();
+        $conn->close();
+    }
+?>
